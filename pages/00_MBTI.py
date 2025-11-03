53d03505dd9151a29dfd4bdf89fdd2dbe291a881
szooyang <szooyang@gmail.com>
2025-11-03 10:35:19 +0900

    Update 00_MBTI.py

--- pages/00_MBTI.py
+++ pages/00_MBTI.py
@@ -1,186 +1,96 @@
 import streamlit as st
 
-# MBTI 추천 데이터
-recommendations = {
-    "INTJ": {
-        "books": [
-            "C: <생각의 탄생>",
-            "C: <총균쇠>"
-        ],
-        "movies": [
-            {"title": "인터스텔라", "poster": "https://m.media-amazon.com/images/I/71nJcZyQHDL._AC_SY679_.jpg"},
-            {"title": "이미테이션 게임", "poster": None}
-        ]
-    },
-    "INTP": {
-        "books": [
-            "C: <코스모스>",
-            "C: <사피엔스>"
-        ],
-        "movies": [
-            {"title": "매트릭스", "poster": "https://m.media-amazon.com/images/I/51EG732BV3L._AC_SY679_.jpg"},
-            {"title": "인셉션", "poster": None}
-        ]
-    },
-    "ENTJ": {
-        "books": [
-            "C: <손자병법>",
-            "C: <칭기즈칸>"
-        ],
-        "movies": [
-            {"title": "월 스트리트", "poster": "https://m.media-amazon.com/images/I/81eR8t2jcFL._AC_SL1500_.jpg"},
-            {"title": "머니볼", "poster": None}
-        ]
-    },
-    "ENTP": {
-        "books": [
-            "C: <호모 데우스>",
-            "C: <괴델, 에셔, 바흐>"
-        ],
-        "movies": [
-            {"title": "아이언맨", "poster": "https://m.media-amazon.com/images/I/81w0Jc7sQ3L._AC_SL1500_.jpg"},
-            {"title": "셜록 홈즈", "poster": None}
-        ]
-    },
-    "INFJ": {
-        "books": [
-            "C: <연금술사>",
-            "C: <데미안>"
-        ],
-        "movies": [
-            {"title": "어바웃 타임", "poster": "https://m.media-amazon.com/images/I/71w+PFl9z0L._AC_SY606_.jpg"},
-            {"title": "인생은 아름다워", "poster": None}
-        ]
-    },
-    "INFP": {
-        "books": [
-            "C: <너의 췌장을 먹고 싶어>",
-            "C: <시네마 천국>"
-        ],
-        "movies": [
-            {"title": "월-E", "poster": "https://m.media-amazon.com/images/I/81z8XZGqerL._AC_SY679_.jpg"},
-            {"title": "코코", "poster": None}
-        ]
-    },
-    "ENFJ": {
-        "books": [
-            "C: <사람을 얻는 기술>",
-            "C: <하버드 사람들은 어떻게 명확하게 말하는가>"
-        ],
-        "movies": [
-            {"title": "파운더", "poster": "https://m.media-amazon.com/images/I/61YNuYeMoDL._AC_SY679_.jpg"},
-            {"title": "히든 피겨스", "poster": None}
-        ]
-    },
-    "ENFP": {
-        "books": [
-            "C: <가벼움의 시대>",
-            "C: <지적 대화를 위한 넓고 얕은 지식>"
-        ],
-        "movies": [
-            {"title": "라라랜드", "poster": "https://m.media-amazon.com/images/I/81jKnz8dOFp._AC_SY679_.jpg"},
-            {"title": "월터의 상상은 현실이 된다", "poster": None}
-        ]
-    },
+st.set_page_config(page_title="MBTI 추천 도서 & 영화 🎬📚")
+
+st.title("📚 MBTI별 책 & 영화 추천 🎬")
+st.write("너의 MBTI를 선택하면 취향저격 추천을 해줄게! 😎")
+
+# MBTI 리스트
+mbti_list = [
+    "ISTJ","ISFJ","INFJ","INTJ",
+    "ISTP","ISFP","INFP","INTP",
+    "ESTP","ESFP","ENFP","ENTP",
+    "ESTJ","ESFJ","ENFJ","ENTJ"
+]
+
+# MBTI별 책/영화 데이터
+recommend_data = {
     "ISTJ": {
-        "books": [
-            "C: <원칙>",
-            "C: <나는 어떻게 일하는가>"
-        ],
-        "movies": [
-            {"title": "덩케르크", "poster": "https://m.media-amazon.com/images/I/91vZt+8CAEL._AC_SL1500_.jpg"},
-            {"title": "체르노빌(시리즈)", "poster": None}
-        ]
+        "books": ["🔹 데미안 - 헤르만 헤세", "🔹 총, 균, 쇠 - 재러드 다이아몬드"],
+        "movies": ["🎬 인셉션", "🎬 셜록 홈즈"]
     },
     "ISFJ": {
-        "books": [
-            "C: <죽은 시인의 사회>",
-            "C: <미움받을 용기>"
-        ],
-        "movies": [
-            {"title": "인턴", "poster": "https://m.media-amazon.com/images/I/71C2q5ogZ0L._AC_SY679_.jpg"},
-            {"title": "월터의 상상은 현실이 된다", "poster": None}
-        ]
+        "books": ["🔹 작은 아씨들 - 루이자 메이 올컷", "🔹 파친코 - 이민진"],
+        "movies": ["🎬 플립", "🎬 겨울왕국"]
     },
-    "ESTJ": {
-        "books": [
-            "C: <성공하는 사람들의 7가지 습관>",
-            "C: <원씽>"
-        ],
-        "movies": [
-            {"title": "미션 임파서블", "poster": "https://m.media-amazon.com/images/I/71MK7pjdAlL._AC_SY879_.jpg"},
-            {"title": "글래디에이터", "poster": None}
-        ]
+    "INFJ": {
+        "books": ["🔹 연금술사 - 파울로 코엘료", "🔹 죽음에 관하여 - 어빈 얄롬"],
+        "movies": ["🎬 어바웃 타임", "🎬 월터의 상상은 현실이 된다"]
     },
-    "ESFJ": {
-        "books": [
-            "C: <하트 시그널>",
-            "C: <말 그릇>"
-        ],
-        "movies": [
-            {"title": "러브 액츄얼리", "poster": "https://m.media-amazon.com/images/I/71+vJkEpQfL._AC_SY679_.jpg"},
-            {"title": "굿윌헌팅", "poster": None}
-        ]
+    "INTJ": {
+        "books": ["🔹 인간 본성에 대하여 - 스티븐 핑커", "🔹 사피엔스 - 유발 하라리"],
+        "movies": ["🎬 인터스텔라", "🎬 매트릭스"]
     },
     "ISTP": {
-        "books": [
-            "C: <오리지널스>",
-            "C: <괴짜 경제학>"
-        ],
-        "movies": [
-            {"title": "007 스카이폴", "poster": "https://m.media-amazon.com/images/I/81GEXZcYH9L._AC_SY679_.jpg"},
-            {"title": "본 시리즈", "poster": None}
-        ]
+        "books": ["🔹 모모 - 미하엘 엔데", "🔹 나는 나로 살기로 했다 - 김수현"],
+        "movies": ["🎬 분노의 질주", "🎬 본 아이덴티티"]
     },
     "ISFP": {
-        "books": [
-            "C: <바람의 그림자>",
-            "C: <모모>"
-        ],
-        "movies": [
-            {"title": "가디언즈 오브 갤럭시", "poster": "https://m.media-amazon.com/images/I/91YQgWcxdRL._AC_SY679_.jpg"},
-            {"title": "500일의 썸머", "poster": None}
-        ]
+        "books": ["🔹 아몬드 - 손원평", "🔹 보건교사 안은영 - 정세랑"],
+        "movies": ["🎬 비긴 어게인", "🎬 라라랜드"]
+    },
+    "INFP": {
+        "books": ["🔹 나미야 잡화점의 기적 - 히가시노 게이고", "🔹 해리 포터 - J.K. 롤링"],
+        "movies": ["🎬 월-E", "🎬 코코"]
+    },
+    "INTP": {
+        "books": ["🔹 코스모스 - 칼 세이건", "🔹 생각에 관한 생각 - 다니엘 카너먼"],
+        "movies": ["🎬 소셜 네트워크", "🎬 트루먼 쇼"]
     },
     "ESTP": {
-        "books": [
-            "C: <부자 아빠 가난한 아빠>",
-            "C: <넛지>"
-        ],
-        "movies": [
-            {"title": "분노의 질주", "poster": "https://m.media-amazon.com/images/I/81HFqRSbVwL._AC_SY679_.jpg"},
-            {"title": "테이큰", "poster": None}
-        ]
+        "books": ["🔹 침묵의 봄 - 레이첼 카슨", "🔹 82년생 김지영 - 조남주"],
+        "movies": ["🎬 아바타", "🎬 007 스카이폴"]
     },
     "ESFP": {
-        "books": [
-            "C: <배움의 발견>",
-            "C: <파티피플>"
-        ],
-        "movies": [
-            {"title": "맘마미아!", "poster": "https://m.media-amazon.com/images/I/81KXOkGg91L._AC_SY879_.jpg"},
-            {"title": "위대한 쇼맨", "poster": None}
-        ]
-    }
+        "books": ["🔹 달러구트 꿈 백화점 - 이미예", "🔹 미드나잇 라이브러리 - 매트 헤이그"],
+        "movies": ["🎬 맘마미아!", "🎬 인사이드 아웃"]
+    },
+    "ENFP": {
+        "books": ["🔹 미움받을 용기 - 기시미 이치로", "🔹 오늘도 펭수, 내일도 펭수"],
+        "movies": ["🎬 해리 포터 시리즈", "🎬 주토피아"]
+    },
+    "ENTP": {
+        "books": ["🔹 총, 균, 쇠 - 재러드 다이아몬드", "🔹 멀티플라이어 - 주창윤"],
+        "movies": ["🎬 아이언맨", "🎬 빅쇼트"]
+    },
+    "ESTJ": {
+        "books": ["🔹 1cm 다이빙 - 태수,문지민", "🔹 트렌드 코리아",],
+        "movies": ["🎬 캡틴 아메리카", "🎬 머니볼"]
+    },
+    "ESFJ": {
+        "books": ["🔹 훈민정음의 길 - 신영복", "🔹 사랑의 기술 - 에리히 프롬"],
+        "movies": ["🎬 인턴", "🎬 사랑과 영혼"]
+    },
+    "ENFJ": {
+        "books": ["🔹 정의란 무엇인가 - 마이클 샌델", "🔹 멈추면, 비로소 보이는 것들 - 혜민"],
+        "movies": ["🎬 굿 윌 헌팅", "🎬 리틀 포레스트"]
+    },
+    "ENTJ": {
+        "books": ["🔹 원씽 - 게리 켈러", "🔹 넛지 - 탈러 & 선스타인"],
+        "movies": ["🎬 다크 나이트", "🎬 킹스맨"]
+    },
 }
 
-st.title("📚 MBTI 책 & 영화 추천 🍿")
+# 사용자 선택
+selected_mbti = st.selectbox("👉 MBTI 선택!", mbti_list)
 
-mbti = st.selectbox("당신의 MBTI는?", list(recommendations.keys()))
+if selected_mbti:
+    st.subheader(f"✨ {selected_mbti}에게 추천하는 책 📚")
+    for b in recommend_data[selected_mbti]["books"]:
+        st.write(b)
 
-if mbti:
-    st.subheader(f"📖 책 추천 for {mbti}")
-    for book in recommendations[mbti]["books"]:
-        st.write(book)
+    st.subheader(f"🎬 {selected_mbti}에게 추천하는 영화 🍿")
+    for m in recommend_data[selected_mbti]["movies"]:
+        st.write(m)
 
-    st.subheader(f"🎬 영화 추천 for {mbti}")
-    
-    # 첫번째 영화 포스터 이미지
-    movie1 = recommendations[mbti]["movies"][0]
-    st.write(movie1["title"])
-    if movie1["poster"]:
-        st.image(movie1["poster"], width=250)
-
-    # 두번째 영화는 텍스트만
-    movie2 = recommendations[mbti]["movies"][1]
-    st.write(movie2["title"])
+    st.write("👉 마음에 드는 작품 하나 골라서 오늘 도전해볼래? 😆🔥")
