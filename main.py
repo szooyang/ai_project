e63c2745040d00fc88db0d3aacf6a8e268a74dda
szooyang <szooyang@gmail.com>
2025-11-04 14:29:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ menu=st.selectbox('좋아하는 음식을 선택해주세요',['치킨','떡볶�
 if st.button('인사말 생성'):
   st.info(name+'님! 안녕하세요!')
   if menu=='치킨':
-     st.warning(menu+'을(를) 좋아하시나봐요!, 저도 좋아해요')
+     st.warning(menu+'을(를) 좋아하시나봐요!, 치킨최고!!)
+  else if menu=='떡볶이':
+    st.write('떡볶이 최고져')
   st.error('반갑습니다!!!')
   st.balloons()
