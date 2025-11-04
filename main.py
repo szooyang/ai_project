fa9d4a4c1e3bc3779b2073f9b7d03d9ae61324f6
szooyang <szooyang@gmail.com>
2025-11-05 08:51:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-name=st.text_input('이름을 입력해주세요!')
-menu=st.selectbox('좋아하는 음식을 선택해주세요',['치킨','떡볶이','바나나'])
-if st.button('인사말 생성'):
-  st.info(name+'님! 안녕하세요!')
-  if menu=='치킨':
-     st.warning(menu+'을(를) 좋아하시나봐요!, 치킨최고!!')
-  else:
-    st.write('떡볶이 최고져')
-  st.error('반갑습니다!!!')
-  st.balloons()
+st.write('안녕하세요, 반갑습니다!')
