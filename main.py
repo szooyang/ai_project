72437c7865a7f959cad1abfd1ea7321ed39f225a
szooyang <szooyang@gmail.com>
2025-11-04 14:28:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ name=st.text_input('이름을 입력해주세요!')
 menu=st.selectbox('좋아하는 음식을 선택해주세요',['치킨','떡볶이','바나나'])
 if st.button('인사말 생성'):
   st.info(name+'님! 안녕하세요!')
-  st.warning(menu+'을(를) 좋아하시나봐요!, 저도 좋아해요')
+  if menu='치킨':
+     st.warning(menu+'을(를) 좋아하시나봐요!, 저도 좋아해요')
   st.error('반갑습니다!!!')
   st.balloons()
